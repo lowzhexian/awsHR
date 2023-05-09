89b0b4aaa34ce187f3f0c6627f7a4dc0dfd12348
lowzhexian <42766954+lowzhexian@users.noreply.github.com>
2023-05-09 23:44:05 +0800

    Update members (1).sql

--- aws/members (1).sql
+++ aws/members (1).sql
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `members`
+-- Table structure for table `staff`
 --
 
 CREATE TABLE `staff` (
